304fa7a07e85c9df21bd3bf88364ffa294e4c9a2
rukersax <berkayturangs@gmail.com>
2026-09-06 21:13:29 +0300

    refactor: fix import paths for savings tip card
    Update relative import paths to correctly resolve to core directory modules.

--- lib/features/subscriptions/presentation/widgets/savings_tip_card.dart
+++ lib/features/subscriptions/presentation/widgets/savings_tip_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/localization/app_localizations.dart';
-import '../../../core/theme/app_theme.dart';
+import '../../../../core/localization/app_localizations.dart';
+import '../../../../core/theme/app_theme.dart';
 
 final savingsTipIndexProvider = StateProvider<int>((ref) => 0);
 
